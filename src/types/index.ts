b8f524a95bd0b2fd227d66f13dbb1b97b7d94d3e
Amin <36745993+4Min4m@users.noreply.github.com>
2025-02-11 17:18:09 +0100

    Create index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export interface Plant {
+  id: number;
+  name: string;
+  price: number;
+  image: string;
+  category: string;
+  description: string;
+}
+
+export interface CartItem extends Plant {
+  quantity: number;
+}
